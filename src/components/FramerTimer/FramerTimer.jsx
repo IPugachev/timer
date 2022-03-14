2419fa3d42d326a392a177685de7c245eee45086
Ilya Pugachev <pi1691a@gmail.com>
2022-03-14 16:05:50 +0800

    final version

--- src/components/FramerTimer/FramerTimer.jsx
+++ src/components/FramerTimer/FramerTimer.jsx
@@ -1,47 +1,64 @@
-import React, { useLayoutEffect, useRef, useState } from 'react'
+import React, { useCallback, useRef, useState } from 'react'
 import './timer.scss'
 import { motion } from 'framer-motion'
 import { MdLightMode } from 'react-icons/md'
 import { MdModeNight } from 'react-icons/md'
 import List from '../List'
+import Bug from '../../assets/bug.svg'
+import Planet from '../../assets/planet.svg'
 
 const FramerTimer = () => {
   console.log('timer rendered')
   console.log('')
+
   const constraintsRef = useRef(null) // FRAMER MOTION
   const [mode, setMode] = useState(true) //MODE
+  const [inputHandler, setInputHandler] = useState(true)
   const [time, setTime] = useState('')
+  const [intervalId, setIntervalId] = useState(null)
+  const [values, setValues] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  })
 
   let interval
-  const startTimer = (endDate) => {
-    // const endDate = new Date().getTime() + 14000
+  const startTimer = () => {
+    clearInterval(intervalId)
+    const endDate =
+      new Date().getTime() +
+      values.days * 24 * 3600 * 1000 +
+      values.hours * 3600 * 1000 +
+      values.minutes * 60 * 1000 +
+      (values.seconds + 1) * 1000
+
     interval = setInterval(() => {
       const startDate = new Date().getTime()
       const now = endDate - startDate
 
       if (now < 0) {
         clearInterval(interval)
+        console.log('cleared')
       } else {
         console.log('interval works')
 
         setTime(now)
       }
     }, 1000)
+    setIntervalId(interval)
+    setInputHandler((prev) => !prev)
   }
-
-  useLayoutEffect(() => {
-    const endDate = new Date().getTime()
-    //+ 600000
-    startTimer(endDate)
-    return () => {
-      clearInterval(interval)
-    }
-  }, [])
 
   const days = Math.floor(time / (24 * 3600 * 1000))
   const hours = Math.floor((time % (24 * 3600 * 1000)) / (3600 * 1000))
   const minutes = Math.floor((time % (3600 * 1000)) / (60 * 1000))
   const seconds = Math.floor((time % (60 * 1000)) / 1000)
+
+  const setTimer = useCallback((format, number) => {
+    setValues((prev) => (prev = { ...prev, [format]: number }))
+  }, [])
+  // console.log(values)
 
   return (
     <div
@@ -52,17 +69,41 @@ const FramerTimer = () => {
       ) : (
         <MdLightMode className='timer__mode-icon timer__mode-icon_light' onClick={() => setMode(true)} />
       )}
-      <div className='timer__input'>
+
+      <motion.img
+        transition={{ duration: 1 }}
+        initial={{ y: -48 }}
+        animate={!inputHandler ? { y: 10 } : { y: -48 }}
+        src={mode ? Bug : Planet}
+        onTap={() => setInputHandler(true)}
+        whileHover={{ rotate: [0, 10, -10, 10, 0] }}
+        alt='bug'
+        className={`timer__image`}
+      />
+
+      <motion.div
+        className='timer__input'
+        initial={{ y: -160 }}
+        animate={inputHandler ? { y: 0 } : { y: -160 }}
+        transition={{ duration: 1 }}>
         <h4>Set time:</h4>
-        <List />
-        <p>days</p>
-        <List />
-        <p>hours</p>
-        <List />
-        <p>min.</p>
-        <List />
-        <p>sec.</p>
-      </div>
+        <div className='timer__input-box'>
+          <List format={'days'} mode={mode} onChange={setTimer} />
+          <p>days</p>
+          <List format={'hours'} mode={mode} onChange={setTimer} />
+          <p>hours</p>
+          <List format={'minutes'} mode={mode} onChange={setTimer} />
+          <p>min.</p>
+          <List format={'seconds'} mode={mode} onChange={setTimer} />
+          <p>sec.</p>
+        </div>
+        <motion.button
+          className={`timer__button ${mode ? 'timer__button_light' : 'timer__button_dark'}`}
+          whileTap={{ scale: 1.3 }}
+          onTap={startTimer}>
+          start
+        </motion.button>
+      </motion.div>
 
       <motion.div
         drag
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,31 +1,31 @@
-import React, { useEffect, useRef, useState } from 'react'
-import Item from '../Item/Item'
+import React, { useEffect, useRef } from 'react'
 import './list.scss'
 
-const List = React.memo(function List() {
-  const [activeItem, setActiveItem] = useState(0)
-  const listRef = useRef()
+const List = React.memo(function List({ format, onChange, mode }) {
   console.log('list rendered')
 
+  const listRef = useRef()
+
   useEffect(() => {
-    if (listRef.current) {
-      const handleScroll = () => setActiveItem(Math.floor(listRef.current.scrollTop / 29))
-      listRef.current.addEventListener('scroll', () => console.log(listRef.current.scrollTop))
-      return () => {
-        listRef.current.removeEventListener('scroll', handleScroll)
-      }
+    const handleScroll = () => {
+      onChange(format, Math.floor(listRef.current.scrollTop / 29) - 2)
     }
+    listRef.current.addEventListener('scroll', handleScroll)
   }, [])
 
-  const test = (item) => {
-    listRef.current.scrollTop = 29 * item
+  const scrollTo = (item) => {
+    listRef.current.scrollTop = 29 * (item + 1)
   }
 
   const numbersList = () => {
     const list = []
     for (let item = 0; item < 60; item++) {
       list.push(
-        <li value={item} className='timer__list-item timer__list-item_active' key={item} onClick={() => test(item)}>
+        <li
+          value={item}
+          className={`timer__list-item ${mode ? 'timer__list-item_light' : 'timer__list-item_dark'}`}
+          key={item}
+          onClick={() => scrollTo(item)}>
           {item < 10 ? '0' + item : item}
         </li>
       )
@@ -35,13 +35,11 @@ const List = React.memo(function List() {
 
   return (
     <ul type='list' className='timer__list' ref={listRef}>
-      <li className='timer__list-item timer__list-item_hidden'>0</li>
-      <li className='timer__list-item timer__list-item_hidden'>0</li>
-
+      <li className='timer__list-item_hidden'>0</li>
+      <li className='timer__list-item_hidden'>0</li>
       {numbersList()}
-
-      <li className='timer__list-item timer__list-item_hidden'>0</li>
-      <li className='timer__list-item timer__list-item_hidden'>0</li>
+      <li className='timer__list-item_hidden'>0</li>
+      <li className='timer__list-item_hidden'>0</li>
     </ul>
   )
 })
